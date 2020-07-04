5eb93f4d32a93491f7d41900e3aba15ebd7dfa57
NewEXE <v.voloshyn96@gmail.com>
2020-07-04 21:37:58 +0300

    First working version
    Features:
    * Score counter
    * Moves left counter
    * Button for restart
    * "Game over" message with restarting when moves out

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+    [SerializeField]
+    private GameObject cardBack;
+
+    [SerializeField]
+    private SceneController sceneController;
+
+    private int _imageId;
+
+    public int imageId {
+        get { return this._imageId; }
+    }
+
+    public void SetCard(int imageId, Sprite image) {
+        this._imageId = imageId;
+        this.GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    private void Reveal() {
+        this.cardBack.SetActive(false);
+        this.sceneController.CardRevealed(this);
+    }
+    
+    public void Unreveal() {
+        this.cardBack.SetActive(true);
+    }
+    
+    private void OnMouseDown() {
+        if (this.cardBack.activeSelf && this.sceneController.CanReveal) {
+            this.Reveal();
+        }
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+    [SerializeField]
+    private SceneController sceneController;
+    
+    [SerializeField]
+    private TextMeshProUGUI scoreLabel;
+    
+    [SerializeField]
+    private TextMeshProUGUI movesLeftLabel;
+    
+    [SerializeField]
+    private GameObject backgound;
+    
+    [SerializeField]
+    private GameObject gameOverCanvas;
+
+    private void Start() {
+        this.SetScore(0);
+    }
+
+    public void OnStartClick() {
+        this.sceneController.Restart();
+    }
+    
+    public void SetScore(int value) {
+        this.scoreLabel.text = "Score: " + value;
+    }
+    
+    public void SetMovesLeft(int value) {
+        this.movesLeftLabel.text = "Moves Left: " + value;
+    }
+
+    public void ShowGameOverLabel() {
+        Vector3 backgroundPos = this.backgound.transform.position;
+        this.backgound.transform.position = new Vector3(backgroundPos.x, backgroundPos.y, -99f);
+
+        this.gameOverCanvas.SetActive(true);
+    }
+}
